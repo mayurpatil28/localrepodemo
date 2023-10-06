c7005a094d936e9e40f1c2080de945f79a4abc6c
git_tutorials <mayur.patil281@gmail.com>
2023-10-06 11:29:23 +0530

    add in main

--- src/localdemo/TestDemo.java
+++ src/localdemo/TestDemo.java
@@ -4,7 +4,7 @@ public class TestDemo {
 
 	public static void main(String[] args) {
 		System.out.println("testt demo");
-		System.out.println("tseting git branch");
+		System.out.println("tseting git branch changes in mail branch");
 	}
 	
 }
